0975abfa974fb132cfb0777fef40c056ff8666d2
Your Name <you@example.com>
2025-09-20 10:45:51 +0500

    fix

--- 03_react_router/src/main.jsx
+++ 03_react_router/src/main.jsx
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, RouterProvider,createBrowserRouter} from 'react-router'
-import App from './App.jsx'
-import { Home, About, Contact, CreateAccount, Error, Signup, Login, User } from "./pages/index.js";
-import './index.css'
-
-const routers = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-         path: "/",
-         element: <Home />
-      },
-      {
-         path: "/about",
-         element: <About />
-      },
-      {
-         path: "/contact",
-         element: <Contact />
-      },
-      {
-         path: "/authpage",
-         element: <CreateAccount />,
-         children: [
-            {
-               path:"signup",
-               element: <Signup />
-            },
-            {
-               path: "login",
-               element: <Login />
-            },
-            {
-               path: "user/:id",
-               element: <User />
-            }
-         ]
-      },
-      {
-         path: "/*",
-         element: <Error />
-      },
-    ]
-  }
-])
-
-
-createRoot(document.getElementById('root')).render(
-
-
-    <StrictMode>
-      <RouterProvider router={routers} />
-    </StrictMode>
-
-)
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createHashRouter } from "react-router"; // 👈 use createHashRouter
@@ -120,4 +59,4 @@ createRoot(document.getElementById("root")).render(
     <RouterProvider router={routers} />
   </StrictMode>
 );
->>>>>>> 84c16d9 (first commit)
+
