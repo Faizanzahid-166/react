efa63d2c36c6187a0534aab03246346b0b1c53f0
Your Name <you@example.com>
2025-09-20 13:58:07 +0500

    fix

--- 07_context_todo/vite.config.js
+++ 07_context_todo/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
-   base: "/react/07_context_api_iii_todo/",
+   base: "/react/07_context_todo/",
   build: {
     rollupOptions: {
       output: {
